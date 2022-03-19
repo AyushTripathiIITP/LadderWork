49f2db79020a6a41c4756ffbf1edc46e2a32abf7
AyushTripathiIITP <101452500+AyushTripathiIITP@users.noreply.github.com>
2022-03-19 11:51:25 +0530

    added the antonAndLetter solution

--- antonAndLetters.cpp
+++ antonAndLetters.cpp
@@ -1,27 +1,14 @@
 #include<bits/stdc++.h>
-using namespace std;
-string s;
-    
-int check(char a , int l){
-    int e(0);
-    for(int x=1 ; x< l ; x+=2 ){
-        if(s[x] != a){
-            ++e;
-        }
-    }
-    cout<<e;
-}
-
+using namespace std; 
 
 int main(){
-    cin>>s;
-    int length;
-    length = (s.size()-1);
-    int e(0);
-    
-    //for(int i=1 ; i<length ; i +=2){
-    e += check(s[1] , length);
-    //}
-    
-    cout<<e;
+    string s;
+    getline(cin,s);
+    set<char> output;
+    for(int i=0 ; i< s.size() ; i++){
+        if(s[i]>= 'a' && s[i]<= 'z')
+        output.insert(s[i]);
+    }
+    cout<< output.size();
+return 0;
 }
